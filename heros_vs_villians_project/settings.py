d0372040b677683ec0d7f7df9735934290eb3add
Your Name <you@example.com>
2022-06-26 16:43:13 -0600

    Added both apps to settings project file along with rest_framework.

--- heros_vs_villians_project/settings.py
+++ heros_vs_villians_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'super_types',
+    'supers',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
