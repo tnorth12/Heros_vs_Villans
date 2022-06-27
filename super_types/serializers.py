b0325b695af845ae346b1a91658c8ee37eb6c037
Your Name <you@example.com>
2022-06-26 20:46:44 -0600

    Added addiontal super hero and attempting to fix query functions in serializer files.

--- super_types/serializers.py
+++ super_types/serializers.py
@@ -4,4 +4,4 @@ from .models import SuperType
 class SuperTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = SuperType
-        fields =['type']
+        fields =['super_type']
