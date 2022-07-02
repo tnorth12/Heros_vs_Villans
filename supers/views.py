d199269920b929ae9ec4a8660fd4d250a0eeef3f
Your Name <you@example.com>
2022-07-02 17:17:33 -0600

    Added custom queries for Hero and Villian and null responses.

--- supers/views.py
+++ supers/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+
+import super_types
+from super_types.models import SuperType
 from .serializers import SuperSerializer
 from .models import Super
 
@@ -11,17 +14,41 @@ from .models import Super
 
 @api_view(['GET', 'POST'])
 def supers_list(request):
-
+    
     if request.method == 'GET':
+        supers_type = request.query_params.get('super_type')                  #query_parma ? to return super_type 
+        # print(supers_type)
+
         supers = Super.objects.all()
+        
+
+        if supers_type:
+            supers = supers.filter(super_type__super_type=supers_type)         #class desingation in each apps model was confusing
+        
+                
         serializer = SuperSerializer(supers, many=True)
         return Response(serializer.data)
-
+    
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # custom_response_dictionary = {}                                                            # Trying this out, but had some difficulty 
+
+    # for super_type in super_types:
+    #     supers = supers.filter(super_type__super_type=super_type)
+    #     serializer = SuperSerializer(supers, many=True)
+
+    #     custom_response_dictionary[super_types.super_type] = {
+    #         "Heros": [],
+    #         "Villians": []
+
+
+    #     }
+       
+   
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def supers_detail(request, pk):
